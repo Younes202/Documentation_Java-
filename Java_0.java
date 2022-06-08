262b63da10fa03cbfbd5482b7b917b37a6f7df50
“Younes <sghyeryounes@gmail.com>
2022-06-09 01:52:14 +0200

    First Programme

---
+++ Java_0.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class java_0 {
+  public static void main(String[] args) {
+      Scanner  input = new Scanner(System.in);
+      System.out.println("Hello can you write  number (a) ");
+      int a = input.nextInt();
+            
+               
+
+         System.out.println("Hello can you write number (b) ");
+               int b = input.nextInt();
+               System.out.println("total is  " +(a+b));
+
+      
+      
+    }
+}
