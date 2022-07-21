6c9119d6325eeb19cbd5ea463cbf0db5072ceee4
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-07-21 23:51:02 +0100

    etoile

---
+++ etoile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class java_9 {
+  public static void main(String[] args) {
+      Scanner  input = new Scanner(System.in);
+       System.out.println("Write N");
+        int et = input.nextInt();
+       for(int i=1;i<=et;i++){
+       for(int j=1;j<=et;j++)
+        {
+            System.out.print("*");
+        }
+       System.out.print("\n");
+           }
+       
+           
+      
+      
+       }
+   
+}
+    
+       
